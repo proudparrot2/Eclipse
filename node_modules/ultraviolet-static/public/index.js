0ad823a734a307f9c4ba9675141018cf4f7dab2c
ParrotDev <71939973+proudparrot2@users.noreply.github.com>
2022-12-17 22:23:59 +0000

    Initial commit

---
+++ node_modules/ultraviolet-static/public/index.js
@@ -0,0 +1,40 @@
+/*const form = document.querySelector('form');
+const input = document.querySelector('input');
+
+form.addEventListener('submit', async event => {
+    event.preventDefault();
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = input.value.trim();
+        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+
+
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    });
+});
+*/
+
+var input = document.getElementById("pageInput")
+var searchBtn = document.getElementById("submit")
+
+searchBtn.addEventListener("click", function() {
+	window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = input.value.trim();
+        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+
+
+        
+	    document.getElementById("frame").src = __uv$config.prefix + __uv$config.encodeUrl(url);
+    });
+
+})
+
+function isUrl(val = ''){
+    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
+    return false;
+};
